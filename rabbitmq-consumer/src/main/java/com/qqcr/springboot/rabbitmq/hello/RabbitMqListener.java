23f915b2b6410888d36e4bcf2d2d6e12bc01cb88
quanqiuchaoren <quanqiuchaoren@gmail.com>
2023-01-02 00:27:39 +0800

    初始化项目

---
+++ rabbitmq-consumer/src/main/java/com/qqcr/springboot/rabbitmq/hello/RabbitMqListener.java
@@ -0,0 +1,16 @@
+package com.qqcr.springboot.rabbitmq.hello;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMqListener {
+
+    @RabbitListener(queues = "boot_queue")
+    public void ListenerQueue(Message message){
+        //System.out.println(message);
+        System.out.println(new String(message.getBody()));
+    }
+
+}
