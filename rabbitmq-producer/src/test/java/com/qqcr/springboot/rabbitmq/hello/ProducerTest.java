edd60d9d18fcdb639e897cfa97b195f13c4dc924
quanqiuchaoren <quanqiuchaoren@gmail.com>
2023-01-02 14:41:05 +0800

    优化代码

--- rabbitmq-producer/src/test/java/com/qqcr/springboot/rabbitmq/hello/ProducerTest.java
+++ rabbitmq-producer/src/test/java/com/qqcr/springboot/rabbitmq/hello/ProducerTest.java
@@ -12,14 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ProducerTest {
-
     //1.注入RabbitTemplate
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     @Test
     public void testSend(){
-
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME,"boot.haha","boot mq hello~~~");
     }
 }
